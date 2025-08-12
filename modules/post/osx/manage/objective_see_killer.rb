33e2839b1239b2faab8024deea29c1c265da4bcb
Corey <gardnerappdevelopment@gmail.com>
2025-08-12 08:10:07 -0400

    Continue writing enumerate module, start parsing xml

--- modules/post/osx/manage/objective_see_killer.rb
+++ modules/post/osx/manage/objective_see_killer.rb
@@ -4,6 +4,7 @@
 ##
 
 class MetasploitModule < Msf::Post
+	require 'nokogiri'
 
 
   def initialize(info = {})
@@ -24,20 +25,52 @@ class MetasploitModule < Msf::Post
   )
   end
 
+  # TODO figure out the directories for all other apps
+  # TODO different versions may have different directory names, figure this out.
   def enumerate
   	# Array of directory names used by Objective See products. These will be in the /Applications/folder.
   	# ex. drwxr-xr-x@  3 root    admin     96 Jun 13  2019 BlockBlock Helper.app
   	# ex. drwxr-xr-x@  3 marvin  admin     96 Jan 29  2019 LuLu.app
-
-  	# TODO figure out the directories for all other apps
-  	# TODO different versions may have different directory names, figure this out.
-  	product_directories = ["BlockBlock Helper.app", "KnockKnock.app", "LuLu.app"].flat_map {|prod| "/Applications/#{prod}"}
+  	products = ["BlockBlock Helper.app", "KnockKnock.app", "LuLu.app"].flat_map {|prod| "/Applications/#{prod}"}
 
   	# get a list of all installed products
-  	@installed_products = []
-  	product_directories.each {|dir| @installed_products << dir if is_dir? dir}
-  	# in /Applications/LuLu.app/Contents/Resources/LuLu Uninstaller.app
+  	@installed = {
+  		:writable => [],
+  		:unwritable => []
+  	}
+
+  	# TODO get versions. They are installed in Product.App/Contents/Info.plist
+  	# CFBundleVersion is the key in the xml file
+
+  	products.each do |dir| 
+  		if is_dir? dir
+  			writable? dir ? @installed[:writable] << dir @installed[:unwirtable] << dir
+  		end
+  	end
+
+  	%i[writable unwritable].each do |status|
+  		@installed[status].each do |dir|
+  			# TODO read Contents/Info.plist file and get version of each app
+  			#plist = dir + "Contents/Info.plist"
+  			#file = File.open plist
+  			#read = File.read file
+  			#doc = Nokogiri::XML::Document.parse read
+
+
+  			print_good "#{dir} is installed and is #{status}"
+  		end 
+  	end 
+
+
+  	@installed
   end
 
-  def exploit; end
+  def disable_LuLu
+
+  end 
+
+  def exploit
+  	print_status("Enumerating Objective See security products.")
+  	enumerate
+  end
 end
